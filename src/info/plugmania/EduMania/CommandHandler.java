fa90d2c06725a29b8b75b035d779dc8631b6ee52
korikisulda <korikisulda@plugmania.info>
2012-06-09 14:52:23 +0100

    added auth command

--- src/info/plugmania/EduMania/CommandHandler.java
+++ src/info/plugmania/EduMania/CommandHandler.java
@@ -4,6 +4,7 @@ package info.plugmania.EduMania;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandHandler{
 	EduMania plugin;
@@ -13,7 +14,7 @@ public class CommandHandler{
 	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 				if(command.getName().equalsIgnoreCase("auth")){
-					
+					if(!plugin.util.authPlayer((Player)sender, plugin.util.join(args, " ", 0)))((Player)sender).kickPlayer("");
 				}
 				return true;
 			}
